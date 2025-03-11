ee229ad5044ccedb841c595451d940ea2f44d328
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-03-11 23:42:28 +0530

    Added project structure

---
+++ src/main/java/lk/ijse/hibernate/serenitymentalhealththerapycenter/AppInitializer.java
@@ -0,0 +1,15 @@
+package lk.ijse.hibernate.serenitymentalhealththerapycenter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.hibernate.serenitymentalhealththerapycenter {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.hibernate.serenitymentalhealththerapycenter to javafx.fxml;
+    exports lk.ijse.hibernate.serenitymentalhealththerapycenter;
+}
