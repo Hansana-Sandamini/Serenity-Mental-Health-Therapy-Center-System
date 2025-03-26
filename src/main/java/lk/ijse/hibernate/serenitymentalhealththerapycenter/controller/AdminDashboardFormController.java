89e77b3cd53e50340d704e1b0cb75d4de2b869db
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-03-26 21:02:25 +0530

    Implement navigateTo method in AdminDashboardFormController

--- src/main/java/lk/ijse/hibernate/serenitymentalhealththerapycenter/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/hibernate/serenitymentalhealththerapycenter/controller/AdminDashboardFormController.java
@@ -2,8 +2,12 @@ package lk.ijse.hibernate.serenitymentalhealththerapycenter.controller;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class AdminDashboardFormController {
 
@@ -31,6 +35,16 @@ public class AdminDashboardFormController {
     @FXML
     private FontAwesomeIcon userIcon;
 
+    public void navigateTo(String fxmlPath) {
+        try {
+            adminDashboardPane.getChildren().clear();
+            AnchorPane load = FXMLLoader.load(getClass().getResource(fxmlPath));
+            adminDashboardPane.getChildren().add(load);
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Fail to load page...!").show();
+        }
+    }
+
     @FXML
     void patientIconOnAction(MouseEvent event) {
 
@@ -53,7 +67,7 @@ public class AdminDashboardFormController {
 
     @FXML
     void therapyProgramIconOnAction(MouseEvent event) {
-
+        navigateTo("/view/TherapyProgramForm.fxml");
     }
 
     @FXML
