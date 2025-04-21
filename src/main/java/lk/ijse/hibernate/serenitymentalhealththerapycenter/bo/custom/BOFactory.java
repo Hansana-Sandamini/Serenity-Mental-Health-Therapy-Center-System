82ee73ce0e0e9470683eb250e5d4f156fd919150
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-04-21 17:22:25 +0530

    Added BOFactory.java file

---
+++ src/main/java/lk/ijse/hibernate/serenitymentalhealththerapycenter/bo/custom/BOFactory.java
@@ -0,0 +1,32 @@
+package lk.ijse.hibernate.serenitymentalhealththerapycenter.bo.custom;
+
+import lk.ijse.hibernate.serenitymentalhealththerapycenter.bo.custom.impl.*;
+
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory() {}
+
+    public static BOFactory getInstance() {
+        if (boFactory == null) {
+            boFactory = new BOFactory();
+        }
+        return boFactory;
+    }
+
+    public enum BOType {
+        USER, THERAPY_PROGRAM, THERAPY_SESSION, THERAPIST, PATIENT, PAYMENT
+    }
+
+    public SuperBO getBO(BOType type) {
+        switch (type) {
+            case USER: return new UserBOImpl();
+            case THERAPY_PROGRAM: return new TherapyProgramBOImpl();
+            case THERAPY_SESSION: return new TherapySessionBOImpl();
+            case THERAPIST: return new TherapistBOImpl();
+            case PATIENT: return new PatientBOImpl();
+            case PAYMENT: return new PaymentBOImpl();
+            default: return null;
+        }
+    }
+}
